c406116b53cc65a4fa17b366a76c8ec7036ac61e
Cem Turan <cemx0121@edu.zealand.dk>
2022-10-06 11:39:36 +0200

    Oprettet en Car klasse med tilhørende properties samt metoder. Lavet en unit test til hver metode i klassen.

---
+++ TicketSystemClassLibraryCarTests/CarTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketSystemClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        Car car = new Car();
+
+        [TestMethod()]
+        public void PriceIsCorrectTest()
+        {
+            // Arrange
+            double expectedValue = 240;
+            double actualValue = car.Price();
+            // Act
+
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeIsCorrectTest()
+        {
+            // Arrange
+            string expectedString = "Car";
+            string actualString = car.VehicleType();
+            // Act
+
+            // Assert
+            Assert.AreEqual(expectedString, actualString);
+        }
+    }
+}
